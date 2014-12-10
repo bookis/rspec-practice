38002c963996f4e3c6994fa7341f73df369d4e57
Bookis Smuin <bookis.smuin@gmail.com>
2014-12-10 09:34:40 -0800

    add email address validation

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  describe "validates" do
+    it 'email address contains an @ symbol' do
+      user = User.new(email: "batexample.com")
+      expect(user.valid?).to eq false
+    end
+  end
+end
